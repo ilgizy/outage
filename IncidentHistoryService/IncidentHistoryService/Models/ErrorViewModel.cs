1db11551b2876c060bcbec0f1d87da6d9d324ac3
Multiname <minefend@gmail.com>
2022-07-06 19:27:26 +0300

    feat: создан проект IncidentHistoryService

---
+++ IncidentHistoryService/IncidentHistoryService/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IncidentHistoryService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
