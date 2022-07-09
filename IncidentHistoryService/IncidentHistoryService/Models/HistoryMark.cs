c4ccd5737809c100dd79546f0b844ac8dde62cd8
Multiname <minefend@gmail.com>
2022-07-09 14:20:23 +0300

    feat(IncidentHistoryService): добавлены модели
    Добавлена модель Incident
    Добавлена модель HistoryMark

---
+++ IncidentHistoryService/IncidentHistoryService/Models/HistoryMark.cs
@@ -0,0 +1,13 @@
+﻿namespace IncidentHistoryService.Models
+{
+    public class HistoryMark
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string Tag { get; set; }
+
+        public int IncidentId { get; set; }
+        public Incident Incident { get; set; }
+    }
+}
---
+++ IncidentHistoryService/IncidentHistoryService/Models/Incident.cs
@@ -0,0 +1,42 @@
+﻿namespace IncidentHistoryService.Models
+{
+    public class Incident
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset? StartDate
+        {
+            get
+            {
+                if (History.Count == 0) return null;
+                return History[0].Date;
+            }
+        }
+        public DateTimeOffset? EndDate
+        {
+            get
+            {
+                if (History.Count == 0) return null;
+                return History.Last().Date;
+            }
+        }
+        public string? LastComment
+        {
+            get
+            {
+                if (History.Count == 0) return null;
+                return History[0].Comment;
+            }
+        }
+        public string UnavailableService { get; set; }
+        public string UnavailableZone { get; set; }
+        public List<string> Tags { get; set; }
+
+        public List<HistoryMark> History { get; set; }
+        public Incident()
+        {
+            Tags = new List<string>();
+            History = new List<HistoryMark>();
+        }
+    }
+}
