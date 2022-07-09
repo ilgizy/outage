73795824c51f78fba36c27fde63a9dfb7e78e04d
Multiname <minefend@gmail.com>
2022-07-09 14:40:23 +0300

    docs(IncidentHistoryService): добавлена документация к моделям
    Добавлена документация к моделям Incident и HistoryMark

--- IncidentHistoryService/IncidentHistoryService/Models/HistoryMark.cs
+++ IncidentHistoryService/IncidentHistoryService/Models/HistoryMark.cs
@@ -1,13 +1,38 @@
 ﻿namespace IncidentHistoryService.Models
 {
+    /// <summary>
+    /// Отметка в истории инцидента
+    /// </summary>
     public class HistoryMark
     {
+        /// <summary>
+        /// Идентификатор отметки
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Комментарий отметки
+        /// </summary>
         public string Comment { get; set; }
+
+        /// <summary>
+        /// Время отметки
+        /// </summary>
         public DateTimeOffset Date { get; set; }
+
+        /// <summary>
+        /// Тег отметки
+        /// </summary>
         public string Tag { get; set; }
 
+        /// <summary>
+        /// Идентификатор инцидента, к которому относится отметка
+        /// </summary>
         public int IncidentId { get; set; }
+
+        /// <summary>
+        /// Инцидент, к которому относится отметка
+        /// </summary>
         public Incident Incident { get; set; }
     }
 }
--- IncidentHistoryService/IncidentHistoryService/Models/Incident.cs
+++ IncidentHistoryService/IncidentHistoryService/Models/Incident.cs
@@ -1,9 +1,27 @@
 ﻿namespace IncidentHistoryService.Models
 {
+    /// <summary>
+    /// Класс инцидента
+    /// </summary>
     public class Incident
     {
+        /// <summary>
+        /// Идентификатор инцидента
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Заголовок инцидента
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Время начала инцидента
+        /// </summary>
+        /// <returns>
+        /// Время первой отметки из History<br/>
+        /// Если History не содержит элементов, вернет null
+        /// </returns>
         public DateTimeOffset? StartDate
         {
             get
@@ -12,6 +30,14 @@
                 return History[0].Date;
             }
         }
+
+        /// <summary>
+        /// Время окончания инцидента
+        /// </summary>
+        /// <returns>
+        /// Время последней отметки из History<br/>
+        /// Если History не содержит элементов, вернет null
+        /// </returns>
         public DateTimeOffset? EndDate
         {
             get
@@ -20,6 +46,14 @@
                 return History.Last().Date;
             }
         }
+
+        /// <summary>
+        /// Последний комментарий
+        /// </summary>
+        /// <returns>
+        /// Комментарий последней отметки из History<br/>
+        /// Если History не содержит элементов, вернет null
+        /// </returns>
         public string? LastComment
         {
             get
@@ -28,11 +62,27 @@
                 return History[0].Comment;
             }
         }
+
+        /// <summary>
+        /// Недоступный во время инцидента сервис
+        /// </summary>
         public string UnavailableService { get; set; }
+
+        /// <summary>
+        /// Недоступная во время инцидента зона
+        /// </summary>
         public string UnavailableZone { get; set; }
+
+        /// <summary>
+        /// Список тегов
+        /// </summary>
         public List<string> Tags { get; set; }
 
+        /// <summary>
+        /// Список отметок для ведения истории инцидента
+        /// </summary>
         public List<HistoryMark> History { get; set; }
+
         public Incident()
         {
             Tags = new List<string>();
