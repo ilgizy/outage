13c2af8538d2442be50aea53dc592f2604524d3e
Razgoeva Lilia <lily.igusheva@gmail.com>
2022-07-10 21:22:45 +0300

    feat(preventive-works): добавлены модели

---
+++ preventive-works/internal/models/PreventiveWork.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type PreventiveWork struct {
+	Id          int       `json:"id"`
+	CreateAt    time.Time `json:"create_at"`
+	Deadline    time.Time `json:"deadline"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CountEvent  int       `json:"count_event"`
+	IdService   int       `json:"id_service"`
+}
