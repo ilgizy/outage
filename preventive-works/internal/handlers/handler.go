17269fccc4dcf025f894782898826dbaf5679651
Razgoeva Lilia <lily.igusheva@gmail.com>
2022-07-11 18:23:58 +0300

    feat(preventive-works):добавлен обработчик /prevntive_works для вывода всех профилактических работ

--- preventive-works/internal/handlers/handler.go
+++ preventive-works/internal/handlers/handler.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"PreventiveWork/internal/models"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type Handler interface {
@@ -9,12 +11,18 @@ type Handler interface {
 }
 
 type handler struct {
+	ds models.DataSource
 }
 
-func NewHandler() Handler {
-	return &handler{}
+func NewHandler(ds models.DataSource) Handler {
+	return &handler{ds: ds}
 }
 
 func (h *handler) Register(router *gin.Engine) {
+	router.GET("/prevntive_works", func(c *gin.Context) {
+		c.Data(http.StatusOK, gin.MIMEJSON, h.ds.GetPreventiveWorkJson())
+		//c.AsciiJSON(http.StatusOK, string(h.ds.GetPreventiveWorkJson()))
+	})
 
+	//router.GET("/prevntive_works/:id")
 }
