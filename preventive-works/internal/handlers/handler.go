6c937cb5fa275607f1271ecd5528e2d8ea698405
Razgoeva Lilia <lily.igusheva@gmail.com>
2022-07-12 14:21:57 +0300

    feat(preventive-works): добавлена обработка ошибок ShowPreventiveWork - ошибка ввода и отсутствия id в списке ShowPreventiveWorks - ошибка отсутствия профилактических работ

--- preventive-works/internal/handlers/handler.go
+++ preventive-works/internal/handlers/handler.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"PreventiveWork/internal/models"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"net/http"
 	"strconv"
 )
@@ -22,19 +24,32 @@ func NewHandler(ds models.DataSource) Handler {
 func (h *handler) Register(router *gin.Engine) {
 
 	router.GET("/preventive_works", h.ShowPreventiveWorks)
-
 	router.GET("/preventive_works/:id", h.ShowPreventiveWork)
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
 
 func (h *handler) ShowPreventiveWork(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("id"))
-	c.Data(http.StatusOK, gin.MIMEJSON, h.ds.FindPreventiveWorkByID(id))
-	return
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	data := h.ds.FindPreventiveWorkByID(id)
+	if data == nil {
+		c.Status(http.StatusNotFound)
+		return
+	}
+	c.Data(http.StatusOK, gin.MIMEJSON, data)
 }
 
 func (h *handler) ShowPreventiveWorks(c *gin.Context) {
+	data := h.ds.GetPreventiveWorkJson()
+	if len(data) == 0 {
+		c.String(http.StatusOK, "Профилактические работы отсутствуют")
+		return
+	}
 	c.Data(http.StatusOK, gin.MIMEJSON, h.ds.GetPreventiveWorkJson())
+
 	//c.AsciiJSON(http.StatusOK, string(h.ds.GetPreventiveWorkJson()))
 }
