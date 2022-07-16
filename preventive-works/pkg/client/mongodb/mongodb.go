62092527990c2878e143b040de384e854b23f031
Razgoeva Lilia <lily.igusheva@gmail.com>
2022-07-16 15:06:08 +0300

    feat(preventive-works): добавлен client к mongodb

---
+++ preventive-works/pkg/client/mongodb/mongodb.go
@@ -0,0 +1,30 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewClient(ctx context.Context, host, port, username, password string) (db *mongo.Client, err error) {
+	var mongoDBURL string
+	mongoDBURL = fmt.Sprintf("mongodb://%s:%s@%s:%s", username, password, host, port)
+
+	clientOptions := options.Client().ApplyURI(mongoDBURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: username,
+		Password: password,
+	})
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to mongoDB due to error: %v", err)
+	}
+
+	if err = client.Ping(ctx, nil); err != nil {
+		return nil, fmt.Errorf("failed to ping mongoDB due to error: %v", err)
+	}
+
+	return client, nil
+}
